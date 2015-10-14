9955f71af858c1add7d644c1bab8e7df3b840512
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-14 11:43:43 +0800

    update color prompt method
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- source/js/colorprompt.js
+++ source/js/colorprompt.js
@@ -15,6 +15,8 @@ $(document).ready(function () {
   var rootHighlight  = span ('<font color=\"crimson\">$1</font><font color=\"lightblue\">$2</font>');
   var promptHighlight  = span ('<font color=\"#FFFF75\">$1 </font>');
 
+  var nothing  = span ('$1 ');
+
   // https://regex101.com/#javascript
   function color_shell_prompt(className) {
     var block = document.getElementsByClassName(className);
@@ -29,18 +31,26 @@ $(document).ready(function () {
       block[i].innerHTML = block[i].innerHTML.replace(/^(\w*)(\s*[:~](.+)\/([^/]+)[#])/, rootHighlight);
       // highlight `hostname directory #' (Gentoo Linux root)
       block[i].innerHTML = block[i].innerHTML.replace(/^(\w*)(\s*\w* [#])/, rootHighlight);
+    }
+  }
+
+  function color_clojure_prompt(className) {
+  var block = document.getElementsByClassName(className);
+    for(var i = 0, l = block.length; i < l; i++) {
       // highlight `user> ` or `user=>` (clojure repl)
       block[i].innerHTML = block[i].innerHTML.replace(/^(user&gt;)\s/mg, promptHighlight);
       block[i].innerHTML = block[i].innerHTML.replace(/^(user>\s)/mg, promptHighlight);
-      block[i].innerHTML = block[i].innerHTML.replace(/^(user=&gt;)\s/mg, promptHighlight);
-      block[i].innerHTML = block[i].innerHTML.replace(/^(user=>\s)/mg, promptHighlight);
-      block[i].innerHTML = block[i].innerHTML.replace(/^(\s\s#_=&gt;)\s/mg, promptHighlight);
-      block[i].innerHTML = block[i].innerHTML.replace(/^(\s\s#_=>\s)/mg, promptHighlight);
+      // this should not change color
+      block[i].innerHTML = block[i].innerHTML.replace(/^(user=&gt;)\s/mg, nothing);
+      block[i].innerHTML = block[i].innerHTML.replace(/^(user=>\s)/mg, nothing);
+      block[i].innerHTML = block[i].innerHTML.replace(/^(\s\s#_=&gt;)\s/mg, nothing);
+      block[i].innerHTML = block[i].innerHTML.replace(/^(\s\s#_=>\s)/mg, nothing);
     }
   }
 
   // color some class with shellprompt
   color_shell_prompt('example');
   color_shell_prompt('src src-sh');
+  color_clojure_prompt('example');
+  color_clojure_prompt('src src-clojure');
 });
-
