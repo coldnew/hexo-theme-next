54fbcadbe05b18bf14a1594dfc2da06f50e8aded
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-02-28 09:54:53 +0800

    remove color-prompt.js
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- utils/src/cljs/blog/core.cljs
+++ utils/src/cljs/blog/core.cljs
@@ -18,3 +18,14 @@
 (defn init []
   (debug-helper)
   (color-prompt))
+
+;; for control toggle visible in 4clojure practice.
+;; #+ATTR_HTML: :class btn btn-default :onclick toggle_visible('p001')
+(defn ^:export toggle-visible [id]
+  (.log js/console (str "toffll2 " id))
+  (let [e (.getElementById js/document id)
+        s (-> e .-style .-display)]
+    (set! (-> e .-style .-display)
+     (if (or (= s "block") (= s ""))
+       "none"
+       "block"))))
