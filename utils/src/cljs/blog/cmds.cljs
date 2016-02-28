1dcccd624b3453ed27dc7f80e7f726d16d48e5f1
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-02-28 11:13:07 +0800

    move exporteds function to cmds namespace
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ utils/src/cljs/blog/cmds.cljs
@@ -0,0 +1,12 @@
+(ns blog.cmds
+  (:require [clojure.string :as str]))
+
+;; for control toggle visible in 4clojure practice.
+;; #+ATTR_HTML: :class btn btn-default :onclick blog.core.toggle_visible('p001')
+(defn ^:export toggle-visible [id]
+  (let [e (.getElementById js/document id)
+        s (-> e .-style .-display)]
+    (set! (-> e .-style .-display)
+          (if (or (= s "block") (= s ""))
+            "none"
+            "block"))))
